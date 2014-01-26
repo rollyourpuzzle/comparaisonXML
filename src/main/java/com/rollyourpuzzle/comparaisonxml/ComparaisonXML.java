7c1816362b7be13694ee339ff70bfbefe98c7ee8
rollyourpuzzle <roll@yourpuzzle.com>
2014-01-26 19:42:41 +0100

    Modification du résultat de la comparaison

--- src/main/java/com/rollyourpuzzle/comparaisonxml/ComparaisonXML.java
+++ src/main/java/com/rollyourpuzzle/comparaisonxml/ComparaisonXML.java
@@ -1,67 +1,52 @@
 package com.rollyourpuzzle.comparaisonxml;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 
 import org.custommonkey.xmlunit.DetailedDiff;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.Difference;
-import org.jdom.JDOMException;
-import org.xml.sax.SAXException;
-
-
 
 public class ComparaisonXML {
-	public static void main(String[] args) throws JDOMException, IOException 
-    {
-        FileReader fr1 = null;
-        int countofdifferences = 0;
-        FileReader fr2 = null;
-        try {
-            fr1 = new FileReader("C:\\xml\\1.xml");
-            fr2 = new FileReader("C:\\xml\\2.xml");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
-        try {
-       
-            Diff lDiff = new Diff(fr1, fr2);
-            MyDifferenceListener lListener = new MyDifferenceListener();
-            lDiff.overrideDifferenceListener(lListener);
-            //System.out.println("Similar? " + lDiff.similar());
-            //System.out.println("Identical? " + lDiff.identical());
+	public static StringBuffer comparaison(String pFile1, String pFile2)
+			throws Exception {
+		FileReader fr1 = null;
+		int countofdifferences = 0;
+		FileReader fr2 = null;
 
-            
-            DetailedDiff detDiff = new DetailedDiff(lDiff);
-           
-           
-            
-            @SuppressWarnings("unchecked")
-			List<Difference> differences = detDiff.getAllDifferences();
-            
-            for (Object object : differences) {
-            	
-            	Difference difference = (Difference) object;         	
-                
-                System.out.println("------------------------");
-                System.out.println(difference.toString());
-                System.out.println("------------------------");
-                countofdifferences++;
-            }
+		fr1 = new FileReader(pFile1);
+		fr2 = new FileReader(pFile2);
 
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		Diff lDiff = new Diff(fr1, fr2);
+		MyDifferenceListener lListener = new MyDifferenceListener();
+		lDiff.overrideDifferenceListener(lListener);
+		// System.out.println("Similar? " + lDiff.similar());
+		// System.out.println("Identical? " + lDiff.identical());
 
+		DetailedDiff detDiff = new DetailedDiff(lDiff);
 
-        System.out.println(countofdifferences);
-        System.exit(0);
-    }
-	
-	   }
+		@SuppressWarnings("unchecked")
+		List<Difference> differences = detDiff.getAllDifferences();
+		
+		StringBuffer lResultat = new StringBuffer();
 
+		for (Object object : differences) {
+
+			Difference difference = (Difference) object;
+
+			lResultat.append("------------------------");
+			lResultat.append("\n");
+			lResultat.append(difference.toString());
+			lResultat.append("\n");
+			lResultat.append("------------------------");
+			lResultat.append("\n");
+			countofdifferences++;
+		}
+		lResultat.append("\n");
+		lResultat.append("Nombre total de différence : " + countofdifferences);
+		
+		return lResultat;
+	}
+
+}
---
+++ src/main/java/com/rollyourpuzzle/comparaisonxml/MyTransferHandler.java
@@ -0,0 +1,104 @@
+package com.rollyourpuzzle.comparaisonxml;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.TransferHandler;
+
+public class MyTransferHandler extends TransferHandler {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3445130075954132086L;
+
+	/**
+	   * @see javax.swing.TransferHandler#canImport(javax.swing.JComponent,
+	   *      java.awt.datatransfer.DataFlavor[])
+	   */
+	  public boolean canImport(JComponent arg0, DataFlavor[] arg1) {
+	    for (int i = 0; i < arg1.length; i++) {
+	      DataFlavor flavor = arg1[i];
+	      if (flavor.equals(DataFlavor.javaFileListFlavor)) {
+	        //System.out.println("canImport: JavaFileList FLAVOR: " + flavor);
+	        return true;
+	      }
+	      if (flavor.equals(DataFlavor.stringFlavor)) {
+	        //System.out.println("canImport: String FLAVOR: " + flavor);
+	        return true;
+	      }
+	      //System.err.println("canImport: Rejected Flavor: " + flavor);
+	    }
+	    // Didn't find any that match, so:
+	    return false;
+	  }
+
+	  /**
+	   * Do the actual import.
+	   * 
+	   * @see javax.swing.TransferHandler#importData(javax.swing.JComponent,
+	   *      java.awt.datatransfer.Transferable)
+	   */
+	  public boolean importData(JComponent comp, Transferable t) {
+	    DataFlavor[] flavors = t.getTransferDataFlavors();
+	    //System.out.println("Trying to import:" + t);
+	    //System.out.println("... which has " + flavors.length + " flavors.");
+	    for (int i = 0; i < flavors.length; i++) {
+	      DataFlavor flavor = flavors[i];
+	      try {
+	        if (flavor.equals(DataFlavor.javaFileListFlavor)) {
+	          //System.out.println("importData: FileListFlavor");
+
+	          List l = (List) t
+	              .getTransferData(DataFlavor.javaFileListFlavor);
+	          Iterator iter = l.iterator();
+	          while (iter.hasNext()) {
+	            File file = (File) iter.next();
+	            if( comp instanceof JTextField) {
+	               JTextField lJTextField = (JTextField) comp;
+	               lJTextField.setText(file.getCanonicalPath());
+	              }
+	            //System.out.println("GOT FILE: "
+	            //    + file.getCanonicalPath());
+	            // Now do something with the file...
+	          }
+	          return true;
+	        } else if (flavor.equals(DataFlavor.stringFlavor)) {
+	          //System.out.println("importData: String Flavor");
+	          String fileOrURL = (String) t.getTransferData(flavor);
+	          //System.out.println("GOT STRING: " + fileOrURL);
+	          try {
+	            URL url = new URL(fileOrURL);
+	            //System.out.println("Valid URL: " + url.toString());
+	            // Do something with the contents...
+	            return true;
+	          } catch (MalformedURLException ex) {
+	            //System.err.println("Not a valid URL");
+	            return false;
+	          }
+	          // now do something with the String.
+
+	        } else {
+	          //System.out.println("importData rejected: " + flavor);
+	          // Don't return; try next flavor.
+	        }
+	      } catch (IOException ex) {
+	        System.err.println("IOError getting data: " + ex);
+	      } catch (UnsupportedFlavorException e) {
+	        System.err.println("Unsupported Flavor: " + e);
+	      }
+	    }
+	    // If you get here, I didn't like the flavor.
+	    Toolkit.getDefaultToolkit().beep();
+	    return false;
+	  }
+}
