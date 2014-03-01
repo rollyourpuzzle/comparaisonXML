0dd217fc1fd8e8cc525a7c09bde2881e5a22c028
rollyourpuzzle <roll@yourpuzzle.com>
2014-03-01 17:38:20 +0100

    comparaisonSansCompteRendu

--- src/main/java/com/rollyourpuzzle/comparaisonxml/ComparaisonXML.java
+++ src/main/java/com/rollyourpuzzle/comparaisonxml/ComparaisonXML.java
@@ -12,23 +12,9 @@ public class ComparaisonXML {
 	public static StringBuffer comparaison(String pFile1, String pFile2)
 
 			throws Exception {
-		FileReader fr1 = null;
 		int countofdifferences = 0;
-		FileReader fr2 = null;
 
-		fr1 = new FileReader(pFile1);
-		fr2 = new FileReader(pFile2);
-
-		Diff lDiff = new Diff(fr1, fr2);
-		MyDifferenceListener lListener = new MyDifferenceListener();
-		lDiff.overrideDifferenceListener(lListener);
-		// System.out.println("Similar? " + lDiff.similar());
-		// System.out.println("Identical? " + lDiff.identical());
-
-		DetailedDiff detDiff = new DetailedDiff(lDiff);
-
-		@SuppressWarnings("unchecked")
-		List<Difference> differences = detDiff.getAllDifferences();
+		List<Difference> differences = compareXML(pFile1, pFile2);
 
 		StringBuffer lResultat = new StringBuffer();
 
@@ -48,6 +34,38 @@ public class ComparaisonXML {
 		lResultat.append("Nombre total de différence : " + countofdifferences);
 		
 		return lResultat;
+	}
+	
+	public static int comparaisonSansCompteRendu(String pFile1, String pFile2)
+
+			throws Exception {
+		
+		List<Difference> differences = compareXML(pFile1, pFile2);
+
+		return differences.size();
+	}
+	
+	private static List<Difference> compareXML(String pFile1, String pFile2) throws Exception{
+		
+		FileReader fr1 = null;
+		FileReader fr2 = null;
+
+		fr1 = new FileReader(pFile1);
+		fr2 = new FileReader(pFile2);
+
+		Diff lDiff = new Diff(fr1, fr2);
+		MyDifferenceListener lListener = new MyDifferenceListener();
+		lDiff.overrideDifferenceListener(lListener);
+		// System.out.println("Similar? " + lDiff.similar());
+		// System.out.println("Identical? " + lDiff.identical());
+
+		DetailedDiff detDiff = new DetailedDiff(lDiff);
+
+		@SuppressWarnings("unchecked")
+		List<Difference> differences = detDiff.getAllDifferences();
+		
+		return differences;
+		
 	}
 
 }
